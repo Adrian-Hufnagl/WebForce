fb93fde01c166de29d0bfbcfa752612dc7cab8b5
e12028169@student.tuwien.ac.at <e12028169@student.tuwien.ac.at>
2023-07-12 16:17:43 +0200

    Listing all Shortcuts with onclock execution

--- gesture.js
+++ gesture.js
@@ -8,8 +8,24 @@ let webcamRunning = false;
 const videoHeight = "270px";
 const videoWidth = "360px";
 
-var gestures = [{
-    Unknown: "&#10067;",
+let leftGesture;
+let leftHistory = [];
+let leftHistoryEl = document.getElementById("leftHistory")
+let leftCounter = 0;
+let leftCounterEl = document.getElementById("leftCounter")
+
+
+let rightGesture;
+let rightHistory = [];
+let rightHistoryEl = document.getElementById("rightHistory")
+let rightCounter = 0;
+let rightCounterEl = document.getElementById("rightCounter")
+
+
+let gestDuration = 10
+
+var gestureList = {
+    None: "&#10067;",
     Closed_Fist: "&#9994;",
     Open_Palm: "&#128400;",
     Pointing_Up: "&#9757;",
@@ -17,7 +33,9 @@ var gestures = [{
     Thumb_Up: "&#128077;",
     Victory: "&#9996;",
     ILoveYou: "&#129311;",
-  }]
+}
+
+
 
 // Before we can use HandLandmarker class we must wait for it to finish
 // loading. Machine Learning models can be large and take a moment to
@@ -35,12 +53,15 @@ async function loadVision() {
 }
 loadVision();
 
- // Continuously grab image from webcam stream and detect it.
+// Continuously grab image from webcam stream and detect it.
 const video = document.getElementById("webcam");
 const canvasElement = document.getElementById("output_canvas");
 const canvasCtx = canvasElement.getContext("2d");
 const leftHandOutput = document.getElementById("gesture-output-left");
 const rightHandOutput = document.getElementById("gesture-output-right");
+const canvasRight = document.getElementById("rightHand");
+const canvasLeft = document.getElementById("leftHand");
+
 // Check if webcam access is supported.
 function hasGetUserMedia() {
     return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
@@ -110,31 +131,49 @@ async function predictWebcam() {
     rightHandOutput.style.width = "100px";
     leftHandOutput.innerText = "-";
     rightHandOutput.innerText = "-";
+    let leftResultSymbol;
+    let leftConfidence;
+    let rightResultSymbol;
+    let rightConfidence;
+
     // Check Handedness and delegate output
+    // CategoryName is inverted because the original video stream is inverted
+    // One Hand -> Ask which one it is
     if (results.gestures.length == 1) {
-        if(results.handednesses[0][0].categoryName == "Left"){
+        let resultSymbol = gestureList[results.gestures[0][0].categoryName];
+        if (results.handednesses[0][0].categoryName == "Right") {
+            leftHandOutput.innerHTML = resultSymbol + " " +
+                Math.round(parseFloat(results.gestures[0][0].score) * 100) +
+                "%";
+            checkGesture(resultSymbol, true);
+        }
+        if (results.handednesses[0][0].categoryName == "Left") {
             rightHandOutput.innerHTML =
-            gestures[0][results.gestures[0][0].categoryName] + " " +
-            Math.round(parseFloat(results.gestures[0][0].score) * 100) +
-            "%";
+                resultSymbol + " " +
+                Math.round(parseFloat(results.gestures[0][0].score) * 100) +
+                "%";
+            checkGesture(resultSymbol, false);
         }
-        if(results.handednesses[0][0].categoryName == "Right"){
-            leftHandOutput.innerHTML =
-            gestures[0][results.gestures[0][0].categoryName] + " " +
-            Math.round(parseFloat(results.gestures[0][0].score) * 100) +
-            "%";
-        }
-        console.log(results)
     }
+
+    // Two Hands -> Ask which one is which
     if (results.gestures.length == 2) {
-        rightHandOutput.innerHTML =
-            gestures[0][results.gestures[0][0].categoryName] + " " +
-            Math.round(parseFloat(results.gestures[0][0].score) * 100) +
-            " %";
-        leftHandOutput.innerHTML =
-            gestures[0][results.gestures[1][0].categoryName] + " " +
-            Math.round(parseFloat(results.gestures[1][0].score) * 100) +
-            " %";
+        if (results.handednesses[0][0].categoryName == "Right") {
+            leftResultSymbol = gestureList[results.gestures[0][0].categoryName];
+            leftConfidence = Math.round(parseFloat(results.gestures[0][0].score) * 100)
+            rightResultSymbol = gestureList[results.gestures[1][0].categoryName];
+            rightConfidence = Math.round(parseFloat(results.gestures[1][0].score) * 100);
+        } 
+        if (results.handednesses[0][0].categoryName == "Left") {
+            leftResultSymbol = gestureList[results.gestures[1][0].categoryName];
+            leftConfidence = Math.round(parseFloat(results.gestures[1][0].score) * 100)
+            rightResultSymbol = gestureList[results.gestures[0][0].categoryName];
+            rightConfidence = Math.round(parseFloat(results.gestures[0][0].score) * 100);
+        }
+        rightHandOutput.innerHTML = rightResultSymbol + " " + rightConfidence + " %";
+        leftHandOutput.innerHTML = leftResultSymbol + " " + leftConfidence + " %";
+        checkGesture(leftResultSymbol, true);
+        checkGesture(rightResultSymbol, false);
     }
 
     // Call this function again to keep predicting when the browser is ready.
@@ -143,5 +182,44 @@ async function predictWebcam() {
     }
 }
 
-function checkGesture(hand){
+function checkGesture(gesture, left) {
+    // No Gesture
+    if (gesture != undefined && gesture != gestureList["None"]) {
+        // If Left Gesture
+        if (left) {
+            console.log("left is true")
+            if (gesture != leftGesture) {
+                leftCounter = 1;
+                leftGesture = gesture;
+            } else {
+                leftCounter++;
+                if (leftCounter == gestDuration) {
+                    leftHistory.push(gesture);
+                    leftHistoryEl.innerHTML = leftHistoryEl.innerHTML + leftGesture;
+                }
+            }
+            // If Right Gesture
+        } else {
+            if (gesture != rightGesture) {
+                rightCounter = 1;
+                rightGesture = gesture;
+            } else {
+                rightCounter++;
+                if (rightCounter == gestDuration) {
+                    rightHistory.push(gesture);
+                    rightHistoryEl.innerHTML = rightHistoryEl.innerHTML + rightGesture;
+                }
+            }
+        }
+    } else {
+        if(left){
+            leftCounter = 0;
+        } else{
+            rightCounter = 0;
+        }
+    }
+    leftCounterEl.innerHTML = leftCounter;
+    rightCounterEl.innerHTML = rightCounter;
 }
+
+module.exports = { gestureList };
--- test.js
+++ test.js
@@ -1,57 +1,137 @@
 const { exec } = require('child_process');
-const { spawn } = require('child_process');
+const { execSync } = require('child_process');
+let yourCommand = 'shortcuts run Test';
+let yourCommand2 = 'shortcuts run "Zoom öffnen"';
+let shortcutArray;
 
+let shortCuts = document.getElementById("shortcutList");
+let shortCutConfigs = []
 
+let shortCutExample = document.getElementsByClassName("shortcut-card")[0]
+shortCutExample.addEventListener('click', function (event) {
+  configGesture(event.currentTarget); // Logs the element that was clicked
+});
 
 let testButton = document.getElementById("testFunctionButton");
 testButton.addEventListener("click", testFunction);
 
 let testButton2 = document.getElementById("testFunctionButton2");
-testButton2.addEventListener("click", testFunction2);
+testButton2.addEventListener("click", configShortcuts);
 
-function testFunction(event){
-    console.log('CMD Test')
-    console.log('Öffne FaceTime')
-    exec('open -a facetime', (err, stdout, stderr) => {
-        if (err) {
-          // Print error
-          console.error(err);
-        } else {
-          // Print results
-          console.log(stdout);
-        }
-      });
+function testFunction(event) {
+  execSync(yourCommand2);
 }
 
-var gestures = [{
-  0: "&#129778;&#10067;&#129777;",
-  1: "&#9994;",
-  2: "&#128400;",
-  3: "&#9757;",
-  4: "&#128077;",
-  5: "&#128078;",
-  6: "&#9996;",
-  7: "&#129311;",
-}]
+var gestureMap = [
+  "&#9994;",
+  "&#128400;",
+  "&#9757;",
+  "&#128077;",
+  "&#128078;",
+  "&#9996;",
+  "&#129311;",
+]
 
 
-function testFunction2(event){
-    //console.log('Kurzbefehle Test')
-    //console.log('Öffne Zoom mit GPT versuch 1')
-    //const child = spawn('shortcuts', ['run', 'AirDrop']);
-    //child.stdout.on('data', (data) => {
-    //  console.log(`stdout: ${data}`);
-    //});
-//
-    //child.stderr.on('data', (data) => {
-    //  console.error(`stderr: ${data}`);
-    //});
-//
-    //child.on('close', (code) => {
-    //  console.log(`child process exited with code ${code}`);
-    //});
-    var el = event.currentTarget;
-    el.innerHTML = gestures[0]["moin"];
-    console.log(gestures[0])
-    console.log(gestures[0][1])
+function configShortcuts(event) {
+  exec('shortcuts list --show-identifiers', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+    } else {
+      let str = stdout;
+      let lines = str.replace(/\r\n/g, '\n').split('\n');
+      let data = lines.map(line => {
+        let match = line.match(/(.*)\s\((.+)\)/);
+        if (match) {
+          return [match[1].trim(), match[2]];
+        } else {
+          return null;
+        }
+      }).filter(item => item !== null);
+      console.log("Shortcut list Output => [Name, ID]");
+      console.log(data);
+      //for (let i = 0; i < data.length; i++) {
+      //  shortCuts.innerText = shortCuts.innerText + "\n" + data[i][0]
+      //}
+      // Initialize 7x7 array with null values
+      shortcutArray = Array.from({ length: 7 }, () => Array(7).fill(null));
+
+      // Fill the array with data
+      for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 7; j++) {
+          let index = i * 7 + j;
+          if (index < data.length) {
+            shortcutArray[i][j] = data[index];
+          }
+        }
+      }
+      console.log("[7]x[7] Array mit null o. 2 Strings");
+      console.log(shortcutArray);
+    }
+    buildShortcutList();
+
+  });
+}
+
+function buildShortcutList() {
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (shortcutArray[i][j] != null) {
+        console.log("generateListElement mit:");
+        console.log(i);
+        console.log(j);
+        generateListElement(i, j)
+      }
+    }
+  }
+}
+
+function generateListElement(x, y) {
+  //console.log("leftGesture")
+  //console.log(gestureMap[x])
+  //console.log("rightGesture")
+  //console.log(gestureMap[y])
+  //console.log(shortcutArray[x][y]);
+  //console.log(shortcutArray[x][y][0]);
+  //console.log(shortcutArray[x][y][1]);
+  //console.log(shortCutExample)
+  let newShortcut = shortCutExample.cloneNode(true)
+  newShortcut.addEventListener('click', function (event) {
+    configGesture(event.currentTarget); // Logs the element that was clicked
+  });
+  //console.log(newShortcut)
+  //console.log(newShortcut.childNodes)
+  //console.log(newShortcut.childNodes[3].childNodes)
+  newShortcut.childNodes[1].innerHTML = shortcutArray[x][y][0];
+  newShortcut.childNodes[3].childNodes[1].innerHTML = gestureMap[x];
+  newShortcut.childNodes[3].childNodes[3].innerHTML = gestureMap[y];
+  shortCuts.appendChild(newShortcut);
+}
+
+function configGesture(e) {
+  console.log("Config Button:")
+  console.log(e)
+  console.log(e.childNodes[1].innerHTML)
+  findCMD(e.childNodes[1].innerHTML)
+
+}
+
+function findCMD(name) {
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (shortcutArray[i][j] != null) {
+        if (shortcutArray[i][j][0] == name) {
+          console.log("shortcutArray");
+          console.log(i);
+          console.log(j);
+          execute(shortcutArray[i][j][1])
+        }
+      }
+    }
+  }
+}
+
+function execute(cmdID) {
+  console.log("shortcuts run " + cmdID)
+  execSync("shortcuts run " + cmdID);
 }
