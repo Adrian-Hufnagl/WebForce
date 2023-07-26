769a5c2b0e0b75e8fe8b9554dcb7f612a9b7c455
e12028169@student.tuwien.ac.at <e12028169@student.tuwien.ac.at>
2023-07-26 14:44:43 +0200

    Readme update

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   const win = new BrowserWindow({
     width: 850,
-    height: 1000,
+    height: 850,
     icon: 'assets/icon.ico',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   })
-  win.webContents.openDevTools()
   win.loadFile('index.html')
 }
 
